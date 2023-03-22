fae1a8ec079c1685947f8ae2fd605c56bbe49385
Juan13x <juang15x@gmail.com>
2023-03-22 13:16:13 -0500

    validation to empty fields. FINiSHED

--- app/src/main/java/com/khenao/notas/ui/main/MainActivity.kt
+++ app/src/main/java/com/khenao/notas/ui/main/MainActivity.kt
@@ -2,24 +2,42 @@ package com.khenao.notas.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.khenao.notas.R
 import com.khenao.notas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mainActivityBinding : ActivityMainBinding
+    private lateinit var mainActivityBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = mainActivityBinding.root
         setContentView(view)
 
-        mainActivityBinding.calculateActionButton.setOnClickListener {
-            val valueNoteOne = mainActivityBinding.enterNoteOneEditText.text.toString().toDouble()
-            val valueNoteTwo = mainActivityBinding.enterNoteTwoEditText.text.toString().toDouble()
-            val valueNoteThree = mainActivityBinding.enterNoteTrheeEditText.text.toString().toDouble()
-            val valueNoteFour = mainActivityBinding.enterNoteFourEditText.text.toString().toDouble()
-            val total = valueNoteOne*0.6 +  valueNoteTwo*0.07 + valueNoteThree*0.08 + valueNoteFour*0.25
+        with(mainActivityBinding) {
 
-            mainActivityBinding.finalGradeTextView.text = total.toString()
+            calculateActionButton.setOnClickListener {
+                val valueGradeOne = enterGradeOneEditText.text.toString()
+                val valueGradeTwo = enterGradeTwoEditText.text.toString()
+                val valueGradeThree = enterGradeTrheeEditText.text.toString()
+                val valueGradeFour = enterGradeFourEditText.text.toString()
+
+                if (
+                    valueGradeOne.isNotEmpty()
+                    and valueGradeTwo.isNotEmpty()
+                    and valueGradeThree.isNotEmpty()
+                    and valueGradeFour.isNotEmpty()
+                ) {
+                    val gradeOne = valueGradeOne.toDouble()
+                    val gradeTwo = valueGradeTwo.toDouble()
+                    val gradeThree = valueGradeThree.toDouble()
+                    val gradeFour = valueGradeFour.toDouble()
+                    val total = gradeOne * 0.6 + gradeTwo * 0.07 + gradeThree * 0.08 + gradeFour * 0.25
+                    finalGradeTextView.text = total.toString()
+                } else {
+                    Toast.makeText(this@MainActivity, R.string.error, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
